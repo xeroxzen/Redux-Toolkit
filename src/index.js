4ea8123babe2a3e62c4316431f588140de849efe
Google Jr <andilembele020@gmail.com>
2022-04-22 21:34:05 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./index.css";
 import App from "./App";
-// import { CreateRoot } from "react-redux/client";
 import { createRoot } from "react-dom/client";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
